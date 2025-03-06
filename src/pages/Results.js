a2db8dc9b3187a10b88d965ae7368a6761516aab
Jakub Błaszyk <jakub.blaszyk@computec.pl>
2025-03-06 10:38:26 +0100

    fix3

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -44,8 +44,11 @@ const Results = () => {
   const calculateStandings = (matches) => {
     const teams = {};
 
-    matches.forEach(({ Team1, Team2, Result1, Result2 }) => {
-      if(Result1 =! null && Result2 != null){
+    matches.forEach(({ Team1, Team2, Result1, Result2, Status }) => {
+     
+      if((Result1 != null && Result2!=null) && Status == "Z"){
+
+        console.log(Team1+"  "+Result1)
       if (!teams[Team1])
         teams[Team1] = { name: Team1, points: 0, goalsFor: 0, goalsAgainst: 0, goalDifference: 0 };
       if (!teams[Team2])
@@ -55,6 +58,9 @@ const Results = () => {
       teams[Team1].goalsAgainst += Result2;
       teams[Team2].goalsFor += Result2;
       teams[Team2].goalsAgainst += Result1;
+
+      teams[Team1].matchesPlayed=teams[Team1].matchesPlayed+1; 
+      teams[Team2].matchesPlayed=teams[Team2].matchesPlayed+1;
 
       if (Result1 > Result2) {
         teams[Team1].points += 3;
@@ -72,7 +78,11 @@ const Results = () => {
 
     return Object.values(teams).sort((a, b) => b.points - a.points || b.goalDifference - a.goalDifference);
   };
-
+  const getTeamMatchesCount = (matches, teamName) => {
+    return matches.filter(({ Team1, Team2, Result1, Result2 }) => 
+        (Team1 === teamName || Team2 === teamName) && Result1 != null && Result2 != null
+    ).length;
+};
   return (
     <div className="p-4 result resF">
       
@@ -88,6 +98,7 @@ const Results = () => {
               <th className="text-center">#</th>
               <th className="text-center">Drużyna</th>
               <th className="text-center">Punkty</th>
+              <th className="text-center">Mecze</th>
               <th className="text-center">B. Strzelone</th>
               <th className="text-center">B. Stracone</th>
               <th className="text-center">B. Bilans</th>
@@ -99,6 +110,7 @@ const Results = () => {
                 <td className="text-center">{index + 1}</td>
                 <td>{team.name}</td>
                 <td className="text-center">{team.points}</td>
+                <td className="text-center">{getTeamMatchesCount(matchData, team.name)}</td>
                 <td className="text-center">{team.goalsFor}</td>
                 <td className="text-center">{team.goalsAgainst}</td>
                 <td className="text-center">{team.goalDifference}</td>
